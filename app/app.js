0ffdfd60d590437c1e1d3b5bf4b1839058110fa0
Joshua Ghali <jjghali@protonmail.ch>
2021-06-17 15:29:09 -0400

    added small nodejs app in order to identify on which pod we currently are when there is a loadbalancer with multiple replicas

--- app/app.js
+++ app/app.js
@@ -3,9 +3,15 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send(`
-    <h1>Hello World!</h1>
-  `);
+    nodeName = `<p><b>Node name: </b>` + process.env.K8_NODE_NAME + `</p>`;
+    podName = `<p><b>Pod name: </b>` + process.env.K8_POD_NAME + `</p>`;
+    namespace = `<p><b>Namespace: </b>` + process.env.K8_POD_NAMESPACE + `</p>`;
+    podIp = `<p><b>IP: </b>` + process.env.K8_POD_IP + `</p>`;
+
+
+    res.send(`
+    <h1>k8s informations</h1>
+  ` + nodeName + podName + namespace + podIp);
 });
 
 app.listen(3000);
